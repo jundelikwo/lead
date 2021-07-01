55428d2b6ed607a543870ad3df4de45dbfeb2241
Jonathan Undelikwo <jundelikwo@gmail.com>
2021-07-01 01:32:00 +0100

    Added analysis & report

--- src/analysis.js
+++ src/analysis.js
@@ -1,10 +1,112 @@
 const { getTrips, getDriver } = require('api');
+const { normalizeAmount } = require('./utils');
 
 /**
  * This function should return the trip data analysis
  *
  * @returns {any} Trip data analysis
  */
-async function analysis() {}
+async function analysis() {
+    let noOfCashTrips = 0;
+    let noOfNonCashTrips = 0;
+    let billedTotal = 0;
+    let cashBilledTotal = 0;
+    let nonCashBilledTotal = 0;
+    let noOfDriversWithMoreThanOneVehicle = 0;
+    let mostTripsByDriver, highestEarningDriver;
+    let drivers = {};
+    
+    const trips = await getTrips();
+
+    trips.forEach((trip) => {
+        const normalizedTripAmount = normalizeAmount(trip.billedAmount);
+
+        if(trip.isCash) {
+            noOfCashTrips++;
+            cashBilledTotal += normalizedTripAmount;
+        } else {
+            noOfNonCashTrips++;
+            nonCashBilledTotal += normalizedTripAmount;
+        }
+
+        billedTotal += normalizedTripAmount;
+
+        // Checks to see if we have already come across this driver
+        if(drivers.hasOwnProperty(trip.driverID)) {
+            // We already have details about this driver in memory
+            // Increment the driver noOfTrips and earning
+
+            const driver = drivers[trip.driverID];
+            driver.noOfTrips++;
+            driver.earnings += normalizedTripAmount;
+        } else {
+            // First time seeing this driver
+            // Create a new record for them
+
+            drivers[trip.driverID] = {
+                noOfTrips: 1,
+                earnings: normalizedTripAmount,
+            };
+        }
+    });
+
+    // This is to ensure that our floating point number have a maximum of 2 decimal places
+    // This is necessary due to how computers handle floating point numbers
+    billedTotal = normalizeAmount(billedTotal.toFixed(2));
+    cashBilledTotal = normalizeAmount(cashBilledTotal.toFixed(2));
+    nonCashBilledTotal = normalizeAmount(nonCashBilledTotal.toFixed(2));
+
+    let mostTrips = 0;
+    let highestEarnings = 0;
+    await Promise.all(Object.keys(drivers).map(async (driverId) => {
+        try {
+            const driver = await getDriver(driverId);
+
+            if(driver.vehicleID.length > 1) {
+                noOfDriversWithMoreThanOneVehicle++;
+            }
+
+            const {noOfTrips, earnings} = drivers[driverId];
+
+            // Note in the trips api, two different drivers had the highest number of trips
+            if (noOfTrips > mostTrips) {
+                mostTrips = noOfTrips;
+                mostTripsByDriver = {
+                    name: driver.name,
+                    email: driver.email,
+                    phone: driver.phone,
+                    noOfTrips: noOfTrips,
+                    totalAmountEarned: earnings,
+                };
+            }
+
+            if (earnings > highestEarnings) {
+                highestEarnings = earnings;
+                highestEarningDriver = {
+                    name: driver.name,
+                    email: driver.email,
+                    phone: driver.phone,
+                    noOfTrips: noOfTrips,
+                    totalAmountEarned: earnings,
+                };
+            }
+        } catch (error) {
+            
+        }
+    }));
+    
+    const result = {
+        noOfCashTrips,
+        noOfNonCashTrips,
+        billedTotal,
+        cashBilledTotal,
+        nonCashBilledTotal,
+        noOfDriversWithMoreThanOneVehicle,
+        mostTripsByDriver,
+        highestEarningDriver,
+    };
+
+    return result;
+}
 
 module.exports = analysis;
--- src/report.js
+++ src/report.js
@@ -1,10 +1,79 @@
 const { getTrips, getDriver, getVehicle } = require('api');
+const { normalizeAmount } = require('./utils');
 
 /**
  * This function should return the data for drivers in the specified format
  *
- * @returns {any} Driver report data
+ * @returns {any} tripDriver report data
  */
-async function driverReport() {}
+async function driverReport() {
+    let drivers = {};
+    
+    const trips = await getTrips();
+
+    trips.forEach(async (trip) => {
+        const normalizedTripAmount = normalizeAmount(trip.billedAmount);
+
+        let tripDriver;
+        if(drivers.hasOwnProperty(trip.driverID)) {
+            tripDriver = drivers[trip.driverID];
+            tripDriver.noOfTrips++;
+            tripDriver.totalAmountEarned += normalizedTripAmount;
+        } else {
+            tripDriver = {
+                id: trip.driverID,
+                noOfTrips: 1,
+                noOfCashTrips: 0,
+                noOfNonCashTrips: 0,
+                totalCashAmount: 0,
+                totalNonCashAmount: 0,
+                totalAmountEarned: normalizedTripAmount,
+                trips: [],
+                vehicles: [],
+            };
+
+            drivers[trip.driverID] = tripDriver;
+        }
+
+        if(trip.isCash) {
+            tripDriver.noOfCashTrips++;
+            tripDriver.totalCashAmount += normalizedTripAmount;
+        } else {
+            tripDriver.noOfNonCashTrips++;
+            tripDriver.totalNonCashAmount += normalizedTripAmount;
+        }
+
+        tripDriver.trips.push({
+            user: trip.user.name,
+            created: trip.created,
+            pickup: trip.pickup.address,
+            destination: trip.destination.address,
+            billed: normalizedTripAmount,
+            isCash: trip.isCash
+        })
+    });
+
+    const driversArray = await Promise.all(Object.values(drivers).map(async (item) => {
+        item.totalAmountEarned = Number(item.totalAmountEarned.toFixed(2));
+        item.totalCashAmount = Number(item.totalCashAmount.toFixed(2));
+        item.totalNonCashAmount = Number(item.totalNonCashAmount.toFixed(2));
+
+        try {
+            const driver = await getDriver(item.id);
+            item.fullName = driver.name;
+            item.phone = driver.phone;
+
+            await Promise.all(driver.vehicleID.map(async (vehicleId) => {
+                const vehicle = await getVehicle(vehicleId);
+                item.vehicles.push({plate: vehicle.plate, manufacturer: vehicle.manufacturer});
+            }))
+        } catch (error) {}
+
+        return item;
+    }))
+    console.log(driversArray)
+    return driversArray;
+}
 
 module.exports = driverReport;
+driverReport();
